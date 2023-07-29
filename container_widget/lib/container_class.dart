1c02a74f13a28533187add62665ef02d1531bbe1
kanishthaaaa <kanishtha3122001@gmail.com>
2023-07-29 16:59:43 +0530

    Widget 3
    This is the simple demonstration of the container widget

--- container_widget/lib/container_class.dart
+++ container_widget/lib/container_class.dart
@@ -1,7 +1,7 @@
 
 // import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+//adding a container class here
 class ContainerWigdet extends StatelessWidget{
   const ContainerWigdet({super.key});
 
